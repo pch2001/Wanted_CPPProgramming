a6d9f94a1db0cc43c9094e3d4b7f176b08e08fa5
박창후 <pencil0303@naver.com>
2026-01-21 16:03:30 +0900

    - 레퍼런스 내용 진행 - 배열 수업 진행

---
+++ Reference/Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+//Call-By-Value
+//값으로 호출
+//"값" 복사
+//void Increment(int value) {
+//	++value;
+//}
+
+//Call-By-Reference
+//참조에 의한 호출
+//참조 = 주소 -> 주소 값 복사
+//void Increment(int* value) {
+//	++(*value);
+//}
+
+//Call-By-Reference
+//참조에 의한 호출
+void Increment(int& value) {
+	++(value);
+}
+
+
+int main()
+{
+	//int a = 5;
+	//int& ref = a;
+	//ref = 10;
+
+	int number = 10;
+	Increment(number);
+
+	std::cin.get();
+}
