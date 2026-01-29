da2fb89b3977d712b5bcd7832dba4f3630d8406e
pch2001 <65890886+pch2001@users.noreply.github.com>
2026-01-29 19:33:47 +0900

    경로 재설정
    Bin, Intermediate 경로 재설정

--- FunctionPointer/Main.cpp
+++ FunctionPointer/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <functional>
 #include <vector>
+#include <array>
 //typedef int (*Adder) (int a, int b);
 
 //using Adder = int(*)(int a, int b);
@@ -23,18 +24,19 @@ class Operator
 public:
 	int Add(int a, int b);
 
-
 };
 
 
 int main()
 {
-
+	std::vector<int> arr;
 	int (*FunctionPointer)();
 	//FunctionPointer = Funtion();
+	std::array<int, 3> a = {};
 
 	std::cout << Function(10, 20, Add) << "\n";
 
-	
+	std::cout << 100;
+
 	std::cin.get();
 }
