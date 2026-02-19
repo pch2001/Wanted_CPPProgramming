725904d86614f8c06cb43079cec6b1d40a07b3d2
pch2001 <65890886+pch2001@users.noreply.github.com>
2026-02-19 09:59:03 +0900

    스택 자료구조 구현 및 데모 진행 (템플릿 버전)
    - Stack 템플릿 버전 구현
    - Main.cpp에서 c랜덤 활용한 데모 진행.

---
+++ DataStruct/5.Stack/Main.cpp
@@ -0,0 +1,53 @@
+#include "Stack/Stack.h"
+
+#include <iostream>
+#include <time.h>
+
+// 랜덤(정수).
+int RandomRange(int min, int max)
+{
+	int diff = (max - min) + 1;
+	return ((diff * rand()) / (RAND_MAX + 1)) + min;
+}
+
+// 랜덤(부동소수).
+float RandomRange(float min, float max)
+{
+	float percent
+		= static_cast<float>(rand())
+		/ static_cast<float>(RAND_MAX);
+	float diff = (max - min);
+	return (percent * diff) + min;
+}
+
+int main()
+{
+	//랜덤 시드
+	srand(time(nullptr));
+
+	const int count = 10;
+	Stack<float, count> stack;
+	
+	for (int ix = 0; ix < count+1; ++ix)
+	{
+		if(!stack.Push(RandomRange(100.0f, 200.0f)))
+		{
+			//예외처리
+			std::cout << "스택에 값 추가 실패"<<"\n";
+		}
+	}
+
+	//스택에서 값 배오면서 출력
+	const int stackCount = stack.Count();
+	for (int ix = 0; ix < stackCount; ++ix)
+	{
+		float value = 0.0f;
+		if (stack.Pop(value))
+		{
+			std::cout << value << "\n";
+		}
+	
+	}
+
+	std::cin.get();
+}
