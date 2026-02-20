479c20a7af49d1c5bc5922cf15c3f01c3b873c06
pch2001 <65890886+pch2001@users.noreply.github.com>
2026-02-20 12:09:30 +0900

    다중 트리 구현 중 - 삭제 재귀 함수 작성 중
    - 재귀 함수에서 삭제할 노드의 자손 노드가 있는 경우의 처리 구현해야 함

--- DataStruct/10.Tree/Container/Node.h
+++ DataStruct/10.Tree/Container/Node.h
@@ -1,8 +1,8 @@
-#pragma once
+﻿#pragma once
 
 #include <vector>
 
-//��� ���� ���� �Լ�
+//노드 삭제 편의 함수
 template <typename T>
 void SafeDelete(T*& t)
 {
@@ -13,19 +13,107 @@ void SafeDelete(T*& t)
 	}
 }
 
-//Ʈ���� ������Ұ� �� ��� Ŭ����
+//트리의 구성요소가 될 노드 클래스
 template<typename T>
 class Node
 {
 public :
 
+	Node() : data(), parent(nullptr)
+	{
+
+	}
+
+	Node(const T& data) : data(data)
+	{
+
+	}
+
+	~Node()
+	{//리소스 정리
+		data = T();
+		parent = nullptr;
+		children.clear();
+	}
+
+public :
+	//값을 전달해 자손을 추가하는 함수
+	//래퍼(Wrapper) 함수 -> 다른 함수를 외부에서 감싸는 형태
+	void AddChild(const T& data)
+	{
+		AddChild(new Node<T>(data));
+	}
+
+	//노드를 직접 전달해 자손 추가하는 함수
+	void AddChild(Node<T>* newChild)
+	{
+		//현재 노드를 부모 노드로 지정
+		newChild->SetParent(this);
+
+		//자손 노드 목록에 새 노드 추가
+		children.emplace_back(newChild);
+
+	}
+
+	//노드 삭제
+	void RemoveChild(Node<T>* child)
+	{
+		//하위 노드까지 순회하면서 노드 검색 후 삭제.
+		//재귀 함수
+		
+	}
+
+
+	//Getter/Setter
+	inline T GetData() const { return data; }
+	inline std::vector<Node<T>*>& GetChildren() const { return childeren; }
+	inline Node<T>* GetPararent() const { return parent; }
+	inline void SetParent(Node<T>* newParent) { parent = new newParent; }
+
 private :
-	//��忡 ������ ������
+	//노드 삭제 재귀 함수
+	//재귀 함수 -> 자기 자신을 호출하는 함수
+	//언제 사용하는가? -> 자기 자신을 호출하면서 데이터의 범위가 줄어들때
+	//주의사항 -> 탈출조건이 항상 있어야 함
+	//주의사항 -> 스택 오버플로우 위험이 있음(콜 스택을 사용하기 때문)
+	//로직 이해하는데는 좋지만, 루프로 로직을 변환하는게 더 좋음
+	void RemoveChildRecursive(Node<T>* child)
+	{
+		if (!child)
+			return;
+		//하위 노드 확인
+		std::vector<Node<T>*>& children = child->GetChildren();
+		
+		//경우의 수1 - 자손이 없는 경우
+		if (children.size() == 0)
+		{
+			//부모 노드의 자손 목록 가져오기
+			auto& parentVector = child->GetPararent()->GetChildren();
+			//자손 목록에서 삭제할 노드 검색(반환 타입은 iterator)
+			auto childIt = std::find(parentVector.begin(), parentVector.end(), child));
+			//자손 목록에서 노드 검색에 성공했으면 해당 노드 제거
+			if (childIt != parentVector.end())
+			{
+				parentVector.erase(childIt);
+			}
+
+			//노드 삭제
+			SafeDelete(child);
+			return;
+		}
+		//경우의 수2 - 자손 노드가 있는 경우
+		//child->GetChildren();
+
+		//마무리
+	}
+
+private :
+	//노드에 저장할 데이터
 	T data;
 
-	//�θ� ���� ���
+	//부모 참조 노드
 	Node<T>* parent = nullptr;
 
-	//�ڼ� ��� ����Ʈ(���� �迭)
+	//자손 노드 리스트(동적 배열)
 	std::vector<Node<T>*> children;
 };
