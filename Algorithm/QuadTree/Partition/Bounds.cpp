a0af4e8f383609f97f1d8289072c7363da75a129
pch2001 <65890886+pch2001@users.noreply.github.com>
2026-03-03 12:08:40 +0900

    쿼드트리
    쿼드 트리 구현중

---
+++ Algorithm/QuadTree/Partition/Bounds.cpp
@@ -0,0 +1,19 @@
+#include "Bounds.h"
+
+bool Bounds::Intersects(const Bounds& other) const
+{
+	// 겹치지 않은 경우 먼저 판단.
+
+	// 다른 영역이 오른쪽으로 벗어난 경우
+	if (other.x > MaxX()) return false;
+	// 다른 영역이 왼쪽으로 벗어난 경우
+	if (other.MaxX() < x) return false;
+	
+	// 다른 영역이 아래쪽으로 벗어난 경우
+	if (other.y > MaxY()) return false;
+	// 다른 영역이 위쪽으로 벗어난 경우
+	if (other.MaxY() < y) return false;
+
+
+	return true;
+}
